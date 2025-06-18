97b7adae288f9a1f2477fcc1c4b2b121490dc0b7
amin <aminrezaaman@gmail.com>
2025-06-18 13:40:53 +0330

    add code

--- Map/GameManager.java
+++ Map/GameManager.java
@@ -2,6 +2,7 @@ package Map;
 
 import Plants.Plant;
 import Plants.Sun;
+import javafx.scene.shape.Rectangle;
 import Zombies.ConeheadZombie;
 import Zombies.Zombie;
 import javafx.animation.KeyFrame;
@@ -29,6 +30,69 @@ public class GameManager {
     private VBox plantMenuVBox;
     private static Label sunPointLabel;
 
+    private GridPane cardSelectionGrid;
+    private VBox selectedCardsBox;
+    private List<Cart> selectedCards = new ArrayList<>();
+    private Runnable onCardsSelectedCallback;
+    private Pane cardSelectionPane;
+
+    public void initializeCardSelection(Runnable onCardsSelected) {
+        cardSelectionPane = new Pane();
+        cardSelectionPane.setPrefSize(600, 400);
+        cardSelectionPane.setLayoutX(100);
+        cardSelectionPane.setLayoutY(100);
+
+        ImageView frame = new ImageView(new Image(getClass().getResourceAsStream("/Plants/Sun/Sun_2.png")));
+        frame.setFitWidth(600);
+        frame.setFitHeight(400);
+        cardSelectionPane.getChildren().add(frame);
+
+        double[][] positions = {
+                {50, 50}, {150, 50}, {250, 50}, {350, 50}, {450, 50},
+                {50, 200}, {150, 200}, {250, 200}, {350, 200}, {450, 200}
+        };
+
+        for (int i = 0; i < 10; i++) {
+            String cardName = "card" + (i + 1);
+            Image img = new Image(getClass().getResourceAsStream("/Plants/Sun/Sun_" + i + ".png"));
+            Cart card = new Cart(cardName, img);
+
+            ImageView cardView = new ImageView(img);
+            cardView.setFitWidth(80);
+            cardView.setFitHeight(100);
+            cardView.setLayoutX(positions[i][0]);
+            cardView.setLayoutY(positions[i][1]);
+
+            cardView.setOnMouseClicked(e -> {
+                if (selectedCards.size() < 5 && !selectedCards.contains(card)) {
+                    selectedCards.add(card);
+
+                    ImageView selectedView = new ImageView(card.getCardImage());
+                    selectedView.setFitWidth(80);
+                    selectedView.setFitHeight(100);
+                    selectedCardsBox.getChildren().add(selectedView);
+
+                    if (selectedCards.size() == 5) {
+                        gamePane.getChildren().remove(cardSelectionPane);
+                        gamePane.getChildren().remove(selectedCardsBox);
+                        if (onCardsSelected != null) onCardsSelected.run();
+                    }
+                }
+            });
+
+            cardSelectionPane.getChildren().add(cardView);
+        }
+
+        selectedCardsBox = new VBox(10);
+        selectedCardsBox.setPrefWidth(150);
+        selectedCardsBox.setLayoutX(20);
+        selectedCardsBox.setLayoutY(100);
+        selectedCardsBox.setStyle("-fx-background-color: rgba(200,200,200,0.7); -fx-padding: 10; -fx-border-color: black;");
+
+        gamePane.getChildren().addAll(cardSelectionPane, selectedCardsBox);
+    }
+
+
 
     public GameManager(Pane gamePane) {
         this.gamePane = gamePane;
@@ -45,11 +109,25 @@ public class GameManager {
 
     }
 
+
+    private void removeCardSelectionUI() {
+        gamePane.getChildren().removeAll(cardSelectionGrid, selectedCardsBox);
+    }
+
+
     private void initializePlantMenu() {
         plantMenuVBox = new VBox(8);
         plantMenuVBox.setLayoutX(0);
-        plantMenuVBox.setLayoutY(85);
-        plantMenuVBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0.3); -fx-padding: 10;");
+        plantMenuVBox.setLayoutY(90);
+       // plantMenuVBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0.3); -fx-padding: 10;");
+
+        plantMenuVBox.setStyle(
+                "-fx-background-color: #8B4513;" +
+                        "-fx-background-radius: 12;" +
+                        "-fx-border-radius: 12;" +
+                        "-fx-padding: 10;" +
+                        "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.3), 10, 0, 0, 4);"
+        );
 
         String[] plantImages = {
                 "/Screen/sun.png",
@@ -66,6 +144,11 @@ public class GameManager {
                 ImageView imageView = new ImageView(image);
                 imageView.setFitWidth(180);
                 imageView.setPreserveRatio(true);
+
+                Rectangle clip = new Rectangle(180, image.getHeight() * (180 / image.getWidth()));
+                clip.setArcWidth(30);
+                clip.setArcHeight(30);
+                imageView.setClip(clip);
 
                 plantMenuVBox.getChildren().add(imageView);
             } catch (Exception e) {
@@ -85,7 +168,7 @@ public class GameManager {
         if (sunPointLabel != null) {
             sunPointLabel.setText("" + sunPoint);
         }
-    }
+    }   
 
     public void buildMap(){
         for (int i = 0; i < map_row; i++) {
